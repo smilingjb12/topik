c9f394ac44a03c31e30d1dc73b0b2aed0e897151
KP <smilingjb12@gmail.com>
2018-03-16 22:43:55 +0300

    landing image

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -7,8 +7,7 @@ import { TestViewComponent } from "@app/home/test-view/test-view.component";
 
 const routes: Routes = [
   Route.withShell([
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent, data: { title: extract('Home') } },
+    { path: '', component: HomeComponent, data: { title: extract('Home') } },
     { path: 'tests/:id', component: TestViewComponent }
   ])
 ];
