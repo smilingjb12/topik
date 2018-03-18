258cad0a838431fb6306045fc08fc22216ca5fd5
KP <smilingjb12@gmail.com>
2018-03-18 14:01:22 +0300

    ga

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -7,7 +7,7 @@ import { TestViewComponent } from "@app/home/test-view/test-view.component";
 
 const routes: Routes = [
   Route.withShell([
-    { path: '', component: HomeComponent, data: { title: extract('Home') } },
+    { path: '', component: HomeComponent },
     { path: 'tests/:id', component: TestViewComponent }
   ])
 ];
